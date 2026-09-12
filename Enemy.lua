a2308b291b55412f844f7895905caef44a94fc5c
Barbara Tobares <barbara.tobares@gmail.com>
2026-09-12 10:55:56 -0300

    primera implementacion de enemy

---
+++ Enemy.lua
@@ -0,0 +1,60 @@
+Enemy = {}
+Enemy.__index = Enemy
+
+function Enemy:new(x, y, width, height, speed, pointA, pointB)
+
+    local enemy = {
+        x = x,
+        y = y,
+        width = width,
+        height = height,
+        speed = speed,
+
+        --puntos de movimiento patrulla
+        pointA = pointA,
+        pointB = pointB,
+
+        --hacia que punto se esta moviendo
+        target = pointB
+    }
+
+    setmetatable(enemy, Enemy)
+
+    return enemy
+end
+
+function Enemy:update(dt)
+
+    local dx = self.target.x - self.x
+    local dy = self.target.y - self.y
+
+    --normalizacion del vector
+    local distance = math.sqrt(dx * dx + dy * dy)
+
+    if distance > 0 then
+
+        local directionX = dx / distance
+        local directionY = dy / distance
+
+        self.x = self.x + directionX * self.speed * dt
+        self.y = self.y + directionY * self.speed * dt
+
+    end
+
+end
+
+function Enemy:draw()
+
+    love.graphics.setColor(1, 0, 0)
+
+    love.graphics.rectangle(
+        "fill",
+        self.x,
+        self.y,
+        self.width,
+        self.height
+    )
+
+    love.graphics.setColor(1, 1, 1)
+
+end
